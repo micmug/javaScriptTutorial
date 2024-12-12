5797f3531ecbfe3d61ec038b20ee1b94cd3ff80a
mimug <mimug77@gmail.com>
2024-12-12 15:08:31 +0100

    javascript: Array Tutorials > finished

--- js-basics/arrays.js
+++ js-basics/arrays.js
@@ -509,10 +509,298 @@ const digits = arrayOfTheRange(-3, 3);
 console.log("result in digits:");
 console.log(digits);
 
-//
+// 17 E2 Includes
 console.log();
 console.log(t(4) + "17 E2 Includes\n");
 
+const num17 = [1, 2, 3, 4, 2, 3, 4];
+const mum17 = 5;
+
+function includeS(array, searchElement) {
+  if (!Array.isArray(array)) {
+    console.log("Die Eingabe ist kein Array.");
+    return 0;
+  }
+
+  let count = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] === searchElement) count++;
+  }
+
+  console.log(
+    "Das Element >" +
+      searchElement +
+      "< ist in dem Array " +
+      count +
+      "-mal vorhanden."
+  );
+  return count;
+}
+
+// includeS(num17x, 4);
+
+// Case 1
+try {
+  includeS(num17, "d");
+} catch (error) {
+  console.log("Ein Array mit diesem Namen existiert nicht.");
+}
+// Case 2
+try {
+  includeS(mum17, "3");
+} catch (error) {
+  console.log("Ein Array mit diesem Namen existiert nicht.");
+}
+// Case 3
+try {
+  includeS(num17, "1");
+} catch (error) {
+  console.log("Ein Array mit diesem Namen existiert nicht.");
+}
+// Case 4
+try {
+  includeS(num17x, "d");
+} catch (error) {
+  console.log("Ein Array mit diesem Namen existiert nicht.");
+}
+
+console.log();
+// Tutorial Solution
+console.log("Tutorial Solution");
+
+function includes(array, searchElement) {
+  for (let element of array) if (element === searchElement) return true;
+  return false;
+}
+
+console.log(includes(num17, 2));
+console.log(includes(num17, 5));
+
+// 18 E3 Except
+console.log();
+console.log(t(4) + "18 E3 Except\n");
+
+const num18 = [1, 2, 3, 4, 5, 6];
+let output = [];
+
+function exclude(array, excluded) {
+  console.log("Original Array: " + array);
+
+  // Stellen Sie sicher, dass 'excluded' immer ein Array ist
+  const excludedArray = Array.isArray(excluded) ? excluded : [excluded];
+
+  const output = array.filter((item) => !excludedArray.includes(item));
+
+  if (output.length === array.length) {
+    console.log("No such element in da array!  -> " + excluded);
+    console.log(array);
+  } else {
+    console.log("Element " + excluded + " has been removed!");
+    console.log(output);
+  }
+  console.log();
+}
+
+exclude(num18, 7);
+exclude(num18, [3, 2, 1, 4]);
+
+// Tutorial Solution
+console.log("Tutorial Solution");
+
+function except(array, excluded) {
+  const output1 = [];
+  for (let element of array)
+    if (!excluded.includes(element)) output1.push(element);
+  return output1;
+}
+
+const output1 = except(num18, [2, 3]);
+console.log(num18);
+console.log(output1);
+
+// 19 E4 Moving an Element
+console.log();
+console.log(t(4) + "19 E4 Moving an Element\n");
+
+const num19 = [1, 2, 3, 4, 5, 6];
+console.log("Original array:", num19);
+
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position < 0 || position >= array.length) {
+    console.error("Invalid offset");
+    return;
+  }
+
+  const output19 = [...array];
+  const element = output19.splice(index, 1)[0];
+  output19.splice(position, 0, element);
+  return output19;
+}
+
+console.log("Move 0 by -4:", move(num19, 0, 4));
+console.log("Move 2 by 1:", move(num19, 2, 1));
+console.log("Move 3 by -2:", move(num19, 3, -2));
+console.log("Move 0 by -2:", move(num19, 0, -2));
+
+// 20 Count
+console.log();
+console.log(t(4) + "20 Count\n");
+
+const num20 = [1, 2, 3, 4, 1, 2, 3, 5, 6, 6, 2, 3, 4, 5, 6, 2, 3];
+console.log("Original array:", num20);
+
+function countOccurrences(array, searchElement) {
+  let count20 = 0;
+  // for (let i = 0; i <= array.length; i++) {
+  //   if (array[i] === searchElement) count20++;
+  // }
+  for (let element of array) {
+    if (element === searchElement) count20++;
+  }
+  console.log("Searched Element: " + searchElement);
+  //return count20;
+  console.log(count20);
+}
+
+function countOccurrencesReduce(array, searchElement) {
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    console.log(t(1), accumulator, t(1), current, t(1), searchElement);
+    return accumulator + occurrence;
+  }, 0);
+}
+
+countOccurrences(num20, 1);
+countOccurrences(num20, 2);
+countOccurrences(num20, 3);
+countOccurrences(num20, 4);
+countOccurrences(num20, 5);
+countOccurrences(num20, 6);
+countOccurrences(num20, 7);
+console.log();
+
+console.log("accumulator, current, searchElement");
+countOccurrencesReduce(num20, 2);
+
+// 21 Get Max
+console.log();
+console.log(t(4) + "21 Get Max\n");
+
+const num21 = [1, 22, 3, 4, 5, 6, 54, 34, 3];
+console.log("Original array:", num21);
+
+// FUNCTION 1
+function getMax1(array) {
+  let max = 0;
+  for (let element of array)
+    element > max ? (max = element) : console.log("curent max: " + max);
+
+  console.log("Greatest number in array: " + max);
+}
+
+console.log("Function 1: ");
+getMax1(num21);
+console.log();
+
+// FUNCTION 2
+
+function getMax2(array) {
+  let max = 0;
+  for (let element of array) {
+    if (element > max) {
+      max = element;
+    }
+  }
+  return max;
+}
+
+console.log("Function 2: ");
+const max2 = getMax2(num21);
+console.log(max2);
+console.log();
+
+// FUNCTION 3
+
+function getMax3(array) {
+  if (array.length === 0) return undefined;
+
+  const max = array.reduce((accumulator, current) =>
+    current > accumulator ? current : accumulator
+  );
+  return max;
+}
+
+console.log("Function 3: ");
+const max3 = getMax3(num21);
+console.log(max3);
+console.log();
+
+// FUNCTION 4
+
+function getMax4(array) {
+  if (array.length === 0) return undefined;
+
+  return array.reduce((acc, cur) => (acc > cur ? acc : cur));
+}
+
+console.log("Function 3: ");
+const max4 = getMax4(num21);
+console.log(max4);
+console.log();
+
+// 22 Movies
+console.log();
+console.log(t(4) + "22 Movies\n");
+
+const movies = [
+  { title: "a", year: 2018, rating: 4.5 },
+  { title: "b", year: 2018, rating: 4.7 },
+  { title: "c", year: 2018, rating: 3 },
+  { title: "d", year: 2017, rating: 4.5 },
+];
+
+// Function 1
+function sortMovies1(movies) {
+  let sorted = movies.filter((movies) => movies.rating >= 4);
+  sorted = sorted.filter((sorted) => sorted.year >= 2018);
+  //sorted = sorted.sort((a, b) => a.title - b.title);
+  sorted = sorted.sort((a, b) => a.title.localeCompare(b.title));
+  let title = sorted.map((sorted) => sorted.title);
+  title.reverse();
+  return title;
+}
+
+console.log("Function 1:");
+const sortedMovies1 = sortMovies1(movies);
+console.log(sortedMovies1);
+
+// Function 2
+function sortMovies2(movies) {
+  return movies
+    .filter((movie) => movie.rating >= 4)
+    .filter((movie) => movie.year >= 2018)
+    .sort((a, b) => a.title.localeCompare(b.title))
+    .reverse()
+    .map((movie) => movie.title);
+}
+
+console.log("Function 2:");
+const sortedMovies2 = sortMovies2(movies);
+console.log(sortedMovies2);
+
+// Function 3
+console.log("Function 3:");
+const titles = movies
+  .filter((m) => m.year === 2018 && m.rating >= 4)
+  .sort((a, b) => a.rating - b.rating)
+  .reverse()
+  .map((m) => m.title);
+
+console.log(titles);
+
 console.log(t(4) + " ");
+console.log();
+console.log();
 console.log();
 console.log();
